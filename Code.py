bb91a1ff8f6222163e41aece852efc0ec7db3ca5
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-06-30 11:35:37 +0800

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,24 +1,23 @@
-#Upload your file on Github (in a repository) and use its Raw link
 import dash
 import dash_bio as dashbio
 from dash import html
-import urllib.request as urlreq
 from dash.dependencies import Input, Output
+
 app = dash.Dash(__name__)
-data = urlreq.urlopen(
-    'https://raw.githubusercontent.com/Aria4201/test/main/alignment_viewer_p53.fasta'
-).read().decode('utf-8')
+
+# Read the FASTA file from the local working directory
+with open('alignment_viewer_p53.fasta', 'r') as file:
+    data = file.read()
 
 app.layout = html.Div([
     dashbio.AlignmentChart(
         id='my-default-alignment-viewer',
         data=data,
-        #colorscale='hydro',   #for changing colour
-        #conservationcolorscale='blackbody',  #for changing colour
-        #showconsensus=False,      #for adding/removing consensus
-        #showconservation=False,  #for adding/removing bar plot
-        #showgap=False,           #for adding/removing bar plot
-
+        # colorscale='hydro',   # for changing color
+        # conservationcolorscale='blackbody',  # for changing color
+        # showconsensus=False,      # for adding/removing consensus
+        # showconservation=False,  # for adding/removing bar plot
+        # showgap=False,           # for adding/removing bar plot
         height=900,
         tilewidth=30,
     ),
@@ -33,5 +32,6 @@ def update_output(value):
     if value is None:
         return 'No data.'
     return str(value)
+
 if __name__ == '__main__':
     app.run_server(debug=True)
